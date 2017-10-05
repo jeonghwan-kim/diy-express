f71260fe07ab5608e3fae00ff4f7908bc77cf6b5
Kim Jeonghwan <ej88ej@gmail.com>
2017-10-05 11:11:11 +0900

    app refactoring

--- app.js
+++ app.js
@@ -1,38 +1,14 @@
-const fs = require('fs')
-const path = require('path')
-const staticServe = require('./modules/static-serve')
+const staticServe = require('./middlewares/static-serve')
+const errors = require('./middlewares/errors')
 const debug = require('./modules/debug')('app')
 const app = require('./modules/application')()
 
 debug('app is initiated')
 
-const index = (req, res, next) => {
-  if (req.url !== '/') return next()
-
-  const publicPath = path.join(__dirname, './public')
-  fs.readFile(`${publicPath}/index.html`, (err, data) => {
-    if (err) throw err
-
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html');
-    res.end(data)
-  })
-}
-
-const error404 = (req, res, next) => {
-  res.statusCode = 404
-  res.end('Not Found')
-}
-
-const error = (err, req, res, next) => {
-  debug('err mw:', err.message || err)
-  res.statusCode = 500
-  res.end()
-}
-
 app.use(staticServe())
-app.use(index)
-app.use(error404)
-app.use(error)
+app.use(require('./routes/hello-world'))
+app.use(require('./routes/index'))
+app.use(errors.error404)
+app.use(errors.error)
 
 module.exports = app
