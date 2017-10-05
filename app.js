4ce5b6605856e58c0e2578240f89230427bdda32
Kim Jeonghwan <ej88ej@gmail.com>
2017-10-05 11:59:07 +0900

    logger mw

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const staticServe = require('./middlewares/static-serve')
 const errors = require('./middlewares/errors')
+const logger = require('./middlewares/logger')
 const debug = require('./modules/debug')('app')
 const app = require('./modules/application')()
 
 debug('app is initiated')
+
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(logger())
+}
 
 app.use(staticServe())
 app.use(require('./routes/hello-world'))
