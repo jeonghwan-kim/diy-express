9be921d34676dba6635a4ab6ecd46085eea7c21f
Kim Jeonghwan <ej88ej@gmail.com>
2017-10-08 20:07:35 +0900

    템플릿 변경

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,18 @@ const debug = require('../modules/debug')('index')
 
 const listPost = (req, res, next) => {
   debug('listPost()')
-  const data = {title: 'Blog'}
+  const data = {
+    title: 'Blog',
+    scriptPath: 'js/index.js'
+  }
   res.render('index', data)
 }
 
 const newPost = (req, res, next) => {
-  const data = {title: 'New | Blog'}
+  const data = {
+    title: 'New Post',
+    scriptPath: 'js/new.js'
+  }
   res.render('new', data)
 }
 
